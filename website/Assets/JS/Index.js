37fe6b441b4a843eccf087fe7467c442cf2a7400
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-10-22 23:18:20 +0100

    custom logo animation

---
+++ website/Assets/JS/Index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Simulate a delay (5 seconds in this case)
+    setTimeout(function () {
+        // Fade out the preloader
+        const preloader = document.querySelector(".preloader");
+        preloader.style.opacity = 0;
+        setTimeout(function () {
+            preloader.style.display = "none";
+        }, 500);
+
+        // Fade in the content
+        const content = document.querySelector(".content");
+        content.style.display = "block";
+        content.style.opacity = 1;
+    }, 5000); // 5000 milliseconds (5 seconds)
+});
