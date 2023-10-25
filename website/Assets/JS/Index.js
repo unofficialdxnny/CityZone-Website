9d0d9c1ccf8a12582d00f0e35290bdb193671e5b
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-10-25 23:11:52 +0100

    custom table style

--- website/Assets/JS/Index.js
+++ website/Assets/JS/Index.js
@@ -16,3 +16,29 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+// Premier League Table
+
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+window.addEventListener("DOMContentLoaded", function () {
+    var iframe = document.getElementById("Iframe");
+    
+    // Function to update the iframe width based on viewport width
+    function updateIframeWidth() {
+      var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+      iframe.style.width = viewportWidth
+      
+      // Adjust the iframe width based on viewport size
+    //   if (viewportWidth <= 375) {
+    //     iframe.style.width = "375px";
+    //   } else {
+    //     iframe.style.width = "100%";
+    //   }
+    }
+    
+    // Call the function on page load and when the window is resized
+    updateIframeWidth();
+    window.addEventListener("resize", updateIframeWidth);
+  });
