0cd647a8db42542df9a1dc86f9bbef2d8249abc8
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-11-05 11:59:36 -0800

    new navbar

--- website/Assets/JS/Index.js
+++ website/Assets/JS/Index.js
@@ -1,9 +1,2 @@
-// preloader animation 
+// Navbar
 
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-      document.querySelector('.preloader').classList.add('active');
-      document.querySelector('.content').classList.remove('hidden');
-    }, 5000); // 5000 milliseconds = 5 seconds
-  });
-  
