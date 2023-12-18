1fdbe1d72c2616ebc9aa9eab3eba6011acf51f98
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-12-18 22:09:36 +0000

    .

--- WEB/Assets/JS/maincards.js
+++ WEB/Assets/JS/maincards.js
@@ -3,13 +3,13 @@ const cardData = [
     imageUrl: 'https://cdn.discordapp.com/attachments/1092434952815120515/1172309026973950074/IMG-20231109-WA0042.jpg?ex=655fd8dc&is=654d63dc&hm=c66844bb1e1a1d06d2bb4dbab566bea35f43b0a397862dcb456fea5ee5c7c877',
     title: 'Rico Lewis Has Burst Onto The Scene In The Last Year And A Half And Helped Manchester City Win The Treble!',
     paragraph: 'Rico Lewis joined the Manchester City academy at the age of eight whilst also being a lifelong blue! He went...',
-    redirectUrl: ''
+    redirectUrl: 'https://cityzone.co.uk/Articles/2'
   },
   {
     imageUrl: 'https://cdn.discordapp.com/attachments/1150106517790527609/1183538451782578216/IMG_0652.jpg?ex=6588b313&is=65763e13&hm=3ea8cab5c62f1107fdaf1a016f282b43594874740e72f9ff8a91ff5cc1b2d4b9',
     title: 'Here Are CityZone’s Player Ratings For Manchester City’s Game Against Luton Town',
     paragraph: 'Here Are CityZone’s Player Ratings For Manchester City’s Game Against Luton Town...',
-    redirectUrl: ''
+    redirectUrl: 'https://cityzone.co.uk/Articles/1'
   },
 
   
