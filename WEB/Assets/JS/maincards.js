af0f750306dfa1b9f6518dc1c795e82af241f7b4
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-12-18 21:36:22 +0000

    index page done

--- WEB/Assets/JS/maincards.js
+++ WEB/Assets/JS/maincards.js
@@ -1,41 +1,46 @@
 const cardData = [
-    {
-      imageUrl: 'https://picsum.photos/200/200',
-      title: 'Card 1',
-      paragraph: 'Description for Card 1.',
-    },
-    {
-      imageUrl: 'https://picsum.photos/200/200',
-      title: 'Card 2',
-      paragraph: 'Description for Card 2.',
-    },
-    // Add more card data as needed
-  ];
+  {
+    imageUrl: 'https://cdn.discordapp.com/attachments/1092434952815120515/1172309026973950074/IMG-20231109-WA0042.jpg?ex=655fd8dc&is=654d63dc&hm=c66844bb1e1a1d06d2bb4dbab566bea35f43b0a397862dcb456fea5ee5c7c877',
+    title: 'Rico Lewis Has Burst Onto The Scene In The Last Year And A Half And Helped Manchester City Win The Treble!',
+    paragraph: 'Rico Lewis joined the Manchester City academy at the age of eight whilst also being a lifelong blue! He went...',
+    redirectUrl: ''
+  },
+  {
+    imageUrl: 'https://cdn.discordapp.com/attachments/1150106517790527609/1183538451782578216/IMG_0652.jpg?ex=6588b313&is=65763e13&hm=3ea8cab5c62f1107fdaf1a016f282b43594874740e72f9ff8a91ff5cc1b2d4b9',
+    title: 'Here Are CityZone’s Player Ratings For Manchester City’s Game Against Luton Town',
+    paragraph: 'Here Are CityZone’s Player Ratings For Manchester City’s Game Against Luton Town...',
+    redirectUrl: ''
+  },
 
-  // Function to create and append cards to the container
-  function createCards() {
-    const cardContainer = document.getElementById('card-container');
+  
+  // Add more card data as needed
+];
 
-    cardData.forEach(data => {
-      const card = document.createElement('div');
-      card.classList.add('card');
+// Function to create and append cards to the container
+function createCards() {
+  const cardContainer = document.getElementById('card-container');
 
-      const image = document.createElement('img');
-      image.src = data.imageUrl;
+  cardData.forEach(data => {
+    const card = document.createElement('a');
+    card.classList.add('card');
+    card.href = data.redirectUrl;
 
-      const cardTitle = document.createElement('h2');
-      cardTitle.innerText = data.title;
+    const image = document.createElement('img');
+    image.src = data.imageUrl;
 
-      const cardParagraph = document.createElement('p');
-      cardParagraph.innerText = data.paragraph;
+    const cardTitle = document.createElement('h2');
+    cardTitle.innerText = data.title;
 
-      card.appendChild(image);
-      card.appendChild(cardTitle);
-      card.appendChild(cardParagraph);
+    const cardParagraph = document.createElement('p');
+    cardParagraph.innerText = data.paragraph;
 
-      cardContainer.appendChild(card);
-    });
-  }
+    card.appendChild(image);
+    card.appendChild(cardTitle);
+    card.appendChild(cardParagraph);
 
-  // Call the function to create and append cards
-  createCards();
+    cardContainer.appendChild(card);
+  });
+}
+
+// Call the function to create and append cards
+createCards();f
